2c3e27d19b976488969a6d9f168b79734b687061
Saltanat <tasalta@gmail.com>
2016-02-08 12:06:46 +0600

    added oauth.js

---
+++ config/oauth.js
@@ -0,0 +1,19 @@
+var ids = {
+facebook: {
+ clientID: '1696841970538599',
+ clientSecret: 'e1cde4105b62535024aa27a3d79d8260',
+ callbackURL: 'http://localhost:9000/auth/facebook/callback'
+},
+google: {
+ clientID      : '204245542037-0aaeui71pn9f00nlfgtf02c59vkb7obj.apps.googleusercontent.com',
+ clientSecret  : 'q0l4RcdKiWcWG1MSsnkm576h',
+ callbackURL   : 'http://localhost:9000/auth/google/callback'
+},
+vk:{
+	clientID: '5090058',
+	clientSecret:'GOKjnWDIAd9tQ7uEbnKw',
+	callbackURL:'http://localhost:9000/auth/vkontakte/callback'
+}
+}
+
+module.exports = ids
